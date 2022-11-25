9f546916104eba2b09de56973bed3e99f323c3b8
Adam <87928802+DnA-IntRicate@users.noreply.github.com>
2022-11-25 17:31:55 +0200

    Revert "Added missing extension for include"
    This reverts commit 45e31123aada03e4e581febce635b733a9a4703b.

--- code/Common/Version.cpp
+++ code/Common/Version.cpp
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assimp/scene.h>
 #include <assimp/version.h>
 
-#include "revision.h.in"
+#include "revision.h"
 
 // --------------------------------------------------------------------------------
 // Legal information string - don't remove this.
